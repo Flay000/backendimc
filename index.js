aa9a92c6801fc55423a1453ef00a05d2e8cec8d5
Flayrys Alves Pariz <flayrys100@gmail.com>
2025-07-29 20:59:38 -0300

    fix cors

--- index.js
+++ index.js
@@ -1,27 +1,31 @@
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
- 
 const cors = require("cors");
 
 const authRoutes = require("./routes/auth");
-// const dataRoutes = require("./routes/data");
 
 const app = express();
-app.use(cors());
+
+// Configura o CORS corretamente
+app.use(cors({
+  origin: "http://localhost:5173", // ou o domínio do seu front no Render
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  credentials: true
+}));
+
 app.use(express.json());
+
+// Rotas
 app.use("/api/auth", authRoutes);
 
-
-  mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+// Conexão com o MongoDB
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("✅ Conectado ao MongoDB"))
   .catch((err) => console.error("❌ Erro ao conectar ao MongoDB:", err));
 
-  const PORT = process.env.PORT || 5000;
-  app.listen(PORT, () => {
-    console.log(`✅ Servidor rodando na porta ${PORT}`);
-  });
-  const authRoute =  require("./routes/auth");
-  app.use("/api/auth", authRoute);
-  // app.use("/api/data", dataRoutes);
-
+// Inicia o servidor
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`✅ Servidor rodando na porta ${PORT}`);
+});
